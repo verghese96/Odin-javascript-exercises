de1294658f700a8c2cf83634417948622aab76c6
verghese96 <verghese96@gmail.com>
2023-03-10 11:35:21 +0800

    Completed Exercises 1-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function(items) {
-
+const removeFromArray = function(Array, ...otherArgs) {
+    var Args =[...otherArgs]; //working
+    var newArray =Array.filter(checkRemoval);
+    
+    function checkRemoval(item){
+        if (!Args.includes(item)){
+            return item;
+        };
+    
+    };
+    return newArray
 };
+//found the following from a stackoverflow post
+// const removeFromArray = function(array, ...args) {
+//     return array.filter(x => !args.includes(x));
+// };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let F=(temp-32)*(5/9);
+  temp= Math.round(F * 10) / 10; //Round off to 1 decimal place
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let C=(temp*(9/5)+32);
+  temp= Math.round(C * 10) / 10; //Round off to 1 decimal place
+  return temp;
 };
-
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
